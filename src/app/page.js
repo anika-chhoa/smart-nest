2adcdc186f0f74dec3a3a35fce33c3f0f2eee41f
anika-chhoa <chhoa.anika07@gmail.com>
2026-06-26 00:04:25 +0600

    add topLocations section in the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Banner from "@/components/shared/Banner";
 import FeaturedProperties from "@/components/shared/FeaturedProperties";
 import RentalStatistics from "@/components/shared/RentalStatistics";
+import TopLocations from "@/components/shared/TopLocations";
 import WhyChooseUs from "@/components/shared/WhyChooseUs";
 
 
@@ -12,6 +13,7 @@ const HomePage = () => {
       <FeaturedProperties/>
       <WhyChooseUs/>
       <RentalStatistics/>
+      <TopLocations/>
     </div>
   );
 };
